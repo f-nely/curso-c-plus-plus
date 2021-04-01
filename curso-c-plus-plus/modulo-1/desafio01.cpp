b2c2e98a950d0830b381ba06aa8b4b6996762665
nely de faria <nelly.nf54@gmail.com>
2021-04-01 18:38:09 -0300

    Adiciona arquivo desafio01.cpp

---
+++ curso-c-plus-plus/modulo-1/desafio01.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	
+	string nome;
+	int idade;
+	float altura;
+	
+	cout << "Informe o nome: ";
+	cin >> nome;
+	
+	cout << "Informe a idade: ";
+	cin >> idade;
+	
+	cout << "Informe a altura: ";
+	cin >> altura;
+	
+	cout << "Nome: " << nome << endl << "idade: " << idade << endl << "altura: " << altura;
+
+	return 0;
+	
+	}

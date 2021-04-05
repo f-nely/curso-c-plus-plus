5755b4da4cb085eaf13cbaf2d15f90ee4c441eec
nely de faria <nelly.nf54@gmail.com>
2021-04-05 18:41:32 -0300

    Adiciona arquivo aula02.cpp

---
+++ curso-c-plus-plus/modulo-1/aula02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	float nota1, nota2, nota3;
+	
+	cout<<"Informe a nota1: ";
+	cin>>nota1;
+	
+	cout<<"Informe a nota2: ";
+	cin>>nota2;
+	
+	cout<<"Informe a nota3: ";
+	cin>>nota3;
+	
+	cout<<"Nota 1: "<<nota1<<endl;
+	cout<<"Nota 2: "<<nota2<<endl;
+	cout<<"Nota 3: "<<nota3<<endl;
+	
+	return 0;
+	}

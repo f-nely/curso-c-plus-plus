d08be693422670b11b9640aa893b52b258d65a96
nely de faria <nelly.nf54@gmail.com>
2021-04-05 18:49:01 -0300

    Adiciona arquivo aula03.cpp

---
+++ curso-c-plus-plus/modulo-1/aula03.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int valor, numero = 10;
+	
+	cout<<"Informe o valor: ";
+	cin>>valor;
+	
+	if (valor == numero) {
+		cout<<"O valor e igual a 10: ";
+	} else {
+		cout<<"O valor e diferente: ";
+	}
+	
+	return 0;
+	}

e356341cbd04ad424f22d5f710a6667a2df7fbf9
nely de faria <nelly.nf54@gmail.com>
2021-04-06 19:34:51 -0300

    Adiciona arquivo desafio003.cpp

---
+++ curso-c-plus-plus/modulo-1/desafio03.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int numero;
+	
+	cout<<"Informe o numero: ";
+	cin>>numero;
+	
+	if (numero % 2 == 0){
+		cout<<"Numero par: ";
+	} else {
+		cout<<"Numero impar: ";
+	}
+	
+	
+}

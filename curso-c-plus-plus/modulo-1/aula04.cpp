bd6bf96316a7ed70c485032be7d5029866e83713
nely de faria <nelly.nf54@gmail.com>
2021-04-06 19:20:05 -0300

    Adiciona arquivo aula04.cpp

---
+++ curso-c-plus-plus/modulo-1/aula04.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int dia;
+	
+	cout<<"Informe o dia da semana: ";
+	cin>>dia;
+	
+	switch(dia) {
+		
+		case 1:
+		
+		cout<<"Domingo: ";
+		
+		break;
+		
+		case 2:
+		
+		cout<<"Segunda: ";
+		
+		break;
+		
+		case 3:
+		
+		cout<<"Terca: ";
+		
+		break;
+		
+		case 4:
+		
+		cout<<"Quarta: ";
+		
+		break;
+		
+		case 5:
+		
+		cout<<"Quinta: ";
+		
+		break;
+		
+		case 6:
+		
+		cout<<"Sexta: ";
+		
+		break;
+		
+		case 7:
+		
+		cout<<"Sabado: ";
+		
+		break;
+		
+		default:
+		
+		cout<<"Entrada invalida: ";
+		
+		break;
+	}
+	
+	return 0;
+	}

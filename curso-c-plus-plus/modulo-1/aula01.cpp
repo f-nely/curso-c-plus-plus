f78f597b9de8f6f145271e133151e44434a3ff31
nely de faria <nelly.nf54@gmail.com>
2021-04-21 13:32:11 -0300

    Definindo setlocale

--- curso-c-plus-plus/modulo-1/aula01.cpp
+++ curso-c-plus-plus/modulo-1/aula01.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
 	
+	setlocale(LC_ALL, "portuguese");
+	
 	/*int numero = 20;
 	float numeroDecimal = 20.45;
 	double numeroGrande = 20.1245;
@@ -16,10 +18,10 @@ int main() {
 	
 	float numero;
 	
-	cout << "Informe um numero: ";
+	cout << "Informe um número: ";
 	cin >> numero;
 	
-	cout << "O numero informado eh: " << numero;
+	cout << "O número informado é: " << numero;
 	
 	return 0;
 }

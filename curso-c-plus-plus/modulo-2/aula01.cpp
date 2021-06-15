7c4846827965ede94c16389355947624a60f2e38
nely de faria <nelly.nf54@gmail.com>
2021-06-15 18:22:20 -0300

    Adiciona arquivo aula01.cpp

---
+++ curso-c-plus-plus/modulo-2/aula01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    char name[20];
+
+    cout << "Informe o nome: ";
+    cin >> name;
+
+    cout << "O seu nome é: " << name << endl;
+    /*char nome[] = "Leo";
+    char nome2[] = {'L', 'e', 'o', 'n', 'a', 'r', 'd', 'o'};
+
+    cout << nome << endl;
+    cout << nome2 <<;*/
+
+    return 0;
+}

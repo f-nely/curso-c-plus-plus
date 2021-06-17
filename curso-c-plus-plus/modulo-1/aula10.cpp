52acf359f27363bb07b21fecdbdbdb635d154fd6
nely de faria <nelly.nf54@gmail.com>
2021-06-17 15:42:09 -0300

    Adiciona arquivo aula10.cpp

---
+++ curso-c-plus-plus/modulo-1/aula10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int num[100], n;
+	
+	cout<< "Informe o tamanho do vetor: ", cin>>n;
+	
+	for(int i = 0; i < n; i++)
+	
+	
+
+
+return 0;	
+	
+}

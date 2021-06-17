4e0cc7e2e16cda4e7e30d257460ce2be789434b6
nely de faria <nelly.nf54@gmail.com>
2021-06-17 16:01:03 -0300

    Outra forma de trabalhar com struct

--- curso-c-plus-plus/modulo-4/aula01_struct.cpp
+++ curso-c-plus-plus/modulo-4/aula01_struct.cpp
@@ -8,11 +8,14 @@ struct Locadora
     char titulo[30];
     int anoLacamento;
 
-}alugar1, alugar2;
+}
+
+alugar1 = {"Velozes e Furiosos", 2020},
+alugar2 = {"Harry Potter", 2018};
 
 int main()
 {
-    cout << "Informe o título do filme: ";
+    /*cout << "Informe o título do filme: ";
     cin.getline(alugar1.titulo, 30);
 
     cout << "Informe o ano do filme: ";
@@ -23,7 +26,15 @@ int main()
     cout << "\n";
 
     cout << "O filme se chama: " << alugar1.titulo << "\n";
-    cout << "O ano de lançamento é: " << alugar1.anoLacamento << "\n";
+    cout << "O ano de lançamento é: " << alugar1.anoLacamento << "\n";*/
+
+    cout << "Dados da Estrutura alugar1: \n";
+    cout << "Nome 1º filme: " << alugar1.titulo << endl;
+    cout << "Ano do 1º filme: " << alugar1.anoLacamento << endl;
+
+    cout << "\nDados da Estrutura alugar2: \n";
+    cout << "Nome 2º filme: " << alugar2.titulo << endl;
+    cout << "Ano do 2º filme: " << alugar2.anoLacamento << endl;
 
     return 0;
 }
